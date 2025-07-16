306eb0b1f2d4888a67a634d41dadf22f6b11234d
Pedro Henriques <pedrohenriques@id.uff.br>
2025-07-16 16:16:32 -0300

    adicionando home page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #importar flask e sqlite3, criar o app, criar função criar banco, definir uma rota e retornar uma função index para renderizar o template e outra rota para cadastro
 # pyright: ignore[reportMissingImports]
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect 
 
 import sqlite3
 
@@ -38,14 +38,25 @@ def cadastrar():
     conn.close()
     return redirect('/lista')
 
-@app.route('/lista')
+
+
+@app.route('/lista', methods=['GET', 'POST'])
 def lista():
     conn = sqlite3.connect("cadastro.db")
     c = conn.cursor()
-    c.execute('SELECT * FROM pessoas') #seleciona todas as linhas da tabela pessoas
+
+    termo = request.args.get('busca', '')
+
+    if termo:
+     c.execute('SELECT * FROM pessoas WHERE nome LIKE ?', (f'%{termo}%',))
+
+
+    else:
+     c.execute('SELECT * FROM pessoas') #seleciona todas as linhas da tabela pessoas
+
     pessoas = c.fetchall() #pega todos os registros e armazena em uma lista de tuplas
     conn.close()
-    return render_template("lista.html", pessoas = pessoas)
+    return render_template("lista.html", pessoas = pessoas, busca = termo)
 
 if __name__ == "__main__":
     criar_banco()
